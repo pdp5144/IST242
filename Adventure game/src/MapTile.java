c20eecfcaa5be03fea9219293c2b41e69eaa745a
pdp5144 <pdp5144@psu.edu>
2023-02-28 12:31:39 -0500

    add

---
+++ Adventure game/src/MapTile.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class MapTile {
+    int x;
+    int y;
+    public MapTile(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MapTile other = (MapTile) obj;
+        if (x != other.x)
+            return false;
+        if (x != other.y)
+            return false;
+        return true;
+    }
+
+
+}
